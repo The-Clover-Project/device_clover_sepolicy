34cd1ab761301f4eea9ee0b43ba7aaca57373c18
RiteshSahany <SahanyRitesh@proton.me>
2026-05-27 14:40:43 +0530

    sepolicy: Update path for Clover
    Change-Id: I8dd2e38ca935b52789be99a75a6dd6e76b2a612a

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -5,11 +5,11 @@
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/clover/sepolicy/atv/vendor
 endif
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/system_ext/private
+   device/clover/sepolicy/atv/system_ext/private
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/system_ext/public
+   device/clover/sepolicy/atv/system_ext/public
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -10,16 +10,16 @@ endif
 endif
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/private
+    device/clover/sepolicy/qcom/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/dynamic \
-    device/lineage/sepolicy/qcom/system
+    device/clover/sepolicy/qcom/dynamic \
+    device/clover/sepolicy/qcom/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/dynamic \
-    device/lineage/sepolicy/qcom/vendor
+    device/clover/sepolicy/qcom/dynamic \
+    device/clover/sepolicy/qcom/vendor
 endif
 
 ifeq (,$(filter device/qcom/sepolicy-legacy-um/legacy/vendor/common device/qcom/sepolicy_vndr/legacy-um/legacy/vendor/common, $(BOARD_VENDOR_SEPOLICY_DIRS)))
